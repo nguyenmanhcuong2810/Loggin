83b7e91aae9ea6202a3547930ab4e537fcb3c13d
Nguyễn Mạnh Cường <132721760+nguyenmanhcuong2810@users.noreply.github.com>
2023-05-10 09:55:40 +0700

    Baitaploggin

---
+++ app/src/main/java/com/example/loggin/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.loggin
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val signUp = findViewById<TextView>(R.id.signUp)
+        val logIn = findViewById<TextView>(R.id.logIn)
+        val signUpLayout = findViewById<View>(R.id.signUpLayout)
+        val logInLayout = findViewById<View>(R.id.logInLayout)
+        val signIn = findViewById<View>(R.id.signIn)
+
+        signUp.setOnClickListener {
+            signUp.background = resources.getDrawable(R.drawable.switch_trcks, null)
+            signUp.setTextColor(resources.getColor(R.color.textColor, null))
+            logIn.background = null
+            signUpLayout.visibility = View.VISIBLE
+            logInLayout.visibility = View.GONE
+            logIn.setTextColor(resources.getColor(R.color.pinkColor, null))
+        }
+
+        logIn.setOnClickListener {
+            signUp.background = null
+            signUp.setTextColor(resources.getColor(R.color.textColor, null))
+            logIn.background = resources.getDrawable(R.drawable.switch_trcks, null)
+            signUpLayout.visibility = View.GONE
+            logInLayout.visibility = View.VISIBLE
+            logIn.setTextColor(resources.getColor(R.color.textColor, null))
+        }
+
+        signIn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, NewActivity::class.java))
+        }
+    }
+}
